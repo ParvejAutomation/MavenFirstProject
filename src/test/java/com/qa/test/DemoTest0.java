c88b811bf50ac3da9bd221e7a2c408ac4f4979f1
ParvejAutomation <parvejalam1009@gmail.com>
2020-05-31 23:01:54 +0530

    2nd Push

---
+++ src/test/java/com/qa/test/DemoTest0.java
@@ -0,0 +1,30 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest0 {
+	@Test
+	public void sum() {
+		System.out.println("Sum");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	@Test
+	public void sub() {
+		System.out.println("Sub");
+		int a=30;
+		int b=20;
+		Assert.assertEquals(10, a-b);
+	}
+	@Test
+	public void mul() {
+		System.out.println("Mul");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+
+}
